cf955da970a9c44ca234a71f273a408f347f5aa6
Jyotidevi001 <92590240+Jyotidevi001@users.noreply.github.com>
2022-11-17 10:21:45 +0000

    Add project files.

---
+++ Logbook3 taks4/Pages/Index.cshtml.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Logbook3_taks4.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+
+        }
+    }
+}
